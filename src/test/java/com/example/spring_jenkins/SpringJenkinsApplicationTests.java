671225672a2ad8ace7c6f7a87a85f1e8b8c316a7
Prasad48 <prasadkanimerakala2@gmail.com>
2024-11-14 16:34:19 +0530

    added all files

---
+++ src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.spring_jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("TestCase executing");
+		assertEquals(true, true);
+	}
+
+}
